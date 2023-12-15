5dc9fd07bfe17f515bc5f8b32471048e529f8bb2
pablogarc <juan.garo29si@gmail.com>
2023-12-15 16:22:45 -0600

    style: update layout app component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import './ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+
+        {/* <footer className="flex items-center justify-center py-10">
+          Learning Next.js 14
+        </footer> */}
+      </body>
     </html>
   );
 }
